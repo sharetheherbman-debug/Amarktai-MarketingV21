dd4064ba7bfe62b6938713b6aabb85efcf232301
sharetheherbman-debug <sharetheherbman@gmail.com>
2026-08-17 17:51:52 +0200

    Limit client studio package to governed StudioClient

--- packages/studio/src/index.js
+++ packages/studio/src/index.js
@@ -1,8 +1,3 @@
 "use client";
 
-export { default as ImageStudio } from './components/ImageStudio';
-export { default as VideoStudio } from './components/VideoStudio';
-export { default as LipSyncStudio } from './components/LipSyncStudio';
-export { default as CinemaStudio } from './components/CinemaStudio';
-export { default as LongFormStudio } from './components/LongFormStudio';
 export { StudioClient } from './client/StudioClient';
